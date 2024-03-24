bcc3565cc9621ebd3e35fbf5d0cbb37afc05c7e2
roman <romanroman1802@gmail.com>
2024-03-24 20:49:04 +0300

    добавлены /help и /start, изменен ответ на сообщение

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from telegram.ext import Application, MessageHandler, filters, CommandHandler
 
-BOTTOKEN = '7166801459:AAFqB5svbsnPg2ASubf11ZKJr-SFip4J5yw'
+BOT_TOKEN = '7166801459:AAFqB5svbsnPg2ASubf11ZKJr-SFip4J5yw'
 
 
-async def echo(update, context):
-    await update.message.reply_text('Я получил сообщение ' + update.message.text)
+async def basic_response(update, context):
+    await update.message.reply_text('Пока не готово...')
 
 
 async def start(update, context):
@@ -17,14 +17,22 @@ async def start(update, context):
     )
 
 
+async def help(update, context):
+    user = update.effective_user
+    await update.message.reply_text(
+        rf"Я бот, который поможет тебе сохранить место, которое тебе понравилось, и"
+        rf" узнать о нём нужную тебе информацию. Сперва сообщением отправь мне название места, и я дам тебе список мест"
+        rf", которые мне удалось для тебя найти. Затем выбери из списка мест то, которое тебе нужно, и я пришлю тебе "
+        rf"информацию о нём. Все очень легко, правда?) А как удобно! Приятного пользования!)"
+    )
+
+
 def main():
-    application = Application.builder().token(BOTTOKEN).build()
+    application = Application.builder().token(BOT_TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
-
-    text_handler = MessageHandler(filters.TEXT, echo)
-
-    application.add_handler(text_handler)
+    application.add_handler(CommandHandler("help", help))
+    application.add_handler(MessageHandler(filters.TEXT, basic_response))
 
     application.run_polling()
 
