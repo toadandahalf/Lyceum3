302d121cde0100d7bf9c5472029ca551d1459a89
NeetAlinee <neetaline@yandex.ru>
2024-03-26 20:25:21 +0300

    коммит номер четыре самый четвертый

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from telegram.ext import Application, MessageHandler, filters, CommandHandler
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 BOT_TOKEN = '7166801459:AAFqB5svbsnPg2ASubf11ZKJr-SFip4J5yw'
 
@@ -9,11 +10,14 @@ async def basic_response(update, context):
 
 async def start(update, context):
     user = update.effective_user
+    reply_keyboard = [['/help'], ['/close']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
     await update.message.reply_html(
-        rf"Привет {user.mention_html()}!) Я бот, который поможет тебе сохранить место, которое тебе понравилось, и"
+        rf"Привет, {user.mention_html()}!) Я бот, который поможет тебе сохранить место, которое тебе понравилось, и"
         rf" узнать о нём нужную тебе информацию. Сперва сообщением отправь мне название места, и я дам тебе список мест"
         rf", которые мне удалось для тебя найти. Затем выбери из списка мест то, которое тебе нужно, и я пришлю тебе "
-        rf"информацию о нём. Все очень легко, правда?) А как удобно! Приятного пользования!)"
+        rf"информацию о нём. Все очень легко, правда?) А как удобно! Приятного пользования!)",
+        reply_markup=markup
     )
 
 
@@ -27,11 +31,19 @@ async def help(update, context):
     )
 
 
+async def close_keyboard(update, context):
+    await update.message.reply_text(
+        "Хорошо, я могу закрыть панель",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler("close", close_keyboard))
     application.add_handler(MessageHandler(filters.TEXT, basic_response))
 
     application.run_polling()
