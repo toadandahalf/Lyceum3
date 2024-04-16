88cbf1e7ebffab0c0afd4e7692d7e9af76b7dc5d
roman <romanroman1802@gmail.com>
2024-04-17 00:04:49 +0300

    двоение функции поиска, осталось сделать для второй части ответ

--- main.py
+++ main.py
@@ -6,19 +6,14 @@ import pprint
 
 BOT_TOKEN = '7166801459:AAFqB5svbsnPg2ASubf11ZKJr-SFip4J5yw'
 apikey = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
-
-
-async def start_name(update, context):
-    await update.message.reply_text('Введите название места')
-    return 1
+kinds = {'house': 'дом', 'street': 'улица', 'metro': 'станция метро',
+         'district': 'район города', 'locality': 'населенный пункт'}
 
 
 async def start(update, context):
     user = update.effective_user
     context.user_data['list_of_some'] = {}
 
-    reply_keyboard = [['/start_name - поиск по имени']]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     await update.message.reply_html(
         f"Привет, {user.mention_html()}! Я бот,\n"
         f"который поможет тебе сохранить место,\n"
@@ -34,11 +29,14 @@ async def start(update, context):
         f"\n"
         f"Все очень легко, правда? А как удобно!\n"
         f"Приятного пользования!\n",
-        reply_markup=markup
     )
 
+    await update.message.reply_text('Введите название места:')
 
-async def get_name(update, context):
+    return 1
+
+
+async def get_name_or_address(update, context):
     search_api_server = "https://search-maps.yandex.ru/v1/"
 
     search_params = {
@@ -53,31 +51,82 @@ async def get_name(update, context):
     json_response = response.json()
 
     pprint.pprint(json_response)
+    print(json_response['features'][0]['properties'])
+    try:
+        choosing_from_ten = ''
 
-    choosing_from_ten = ''
+        for i in range(len(json_response['features'])):
+            context.user_data['list_of_some'][str(i)] = json_response["features"][i]
 
-    for i in range(len(json_response['features'])):
-        context.user_data['list_of_some'][str(i)] = json_response["features"][i]
+            choosing_from_ten += f'{i + 1}:\n' \
+                                 f'    Название: {context.user_data["list_of_some"][str(i)]["properties"]["CompanyMetaData"]["name"]}\n' \
+                                 f'    Адрес: {context.user_data["list_of_some"][str(i)]["properties"]["CompanyMetaData"]["address"]}\n' \
+                                 f'    \n'
 
-        choosing_from_ten += f'{i + 1}:\n' \
-                             f'    Название: {context.user_data["list_of_some"][str(i)]["properties"]["CompanyMetaData"]["name"]}\n' \
-                             f'    Адрес: {context.user_data["list_of_some"][str(i)]["properties"]["CompanyMetaData"]["address"]}\n' \
-                             f'    \n'
+        await update.message.reply_html(choosing_from_ten)
 
-    await update.message.reply_html(choosing_from_ten)
+        select_kb_reply = ReplyKeyboardMarkup([[str(i + 1)] for i in range(len(context.user_data["list_of_some"]))],
+                                              one_time_keyboard=True)
 
-    select_kb_reply = ReplyKeyboardMarkup([[str(i + 1)] for i in range(len(context.user_data["list_of_some"]))],
-                                          one_time_keyboard=True)
+        await update.message.reply_html(
+            'Выберите подходящую организацию и отправьте число от 1 до 10,'
+            ' чтобы получить точную информацию об этом месте',
+            reply_markup=select_kb_reply
+        )
 
-    await update.message.reply_html(
-        'Выберите подходящее место и отправьте число от 1 до 10, чтобы получить точную ифнормацию об этом месте',
-        reply_markup=select_kb_reply
-    )
+        return 2
 
-    return 2
+    except KeyError:
+
+        choosing_from_ten = ''
+
+        for i in range(len(json_response['features'])):
+            context.user_data['list_of_some'][str(i)] = json_response["features"][i]
+
+            choosing_from_ten += f'{i + 1}:\n' \
+                                 f'    Тип: {kinds[context.user_data["list_of_some"][str(i)]["properties"]["GeocoderMetaData"]["kind"]]}\n' \
+                                 f'    Адрес: {context.user_data["list_of_some"][str(i)]["properties"]["name"]}, ' \
+                                 f'{context.user_data["list_of_some"][str(i)]["properties"]["description"]}\n'
+
+        await update.message.reply_html(choosing_from_ten)
+
+        select_kb_reply = ReplyKeyboardMarkup([[str(i + 1)] for i in range(len(context.user_data["list_of_some"]))],
+                                              one_time_keyboard=True)
+
+        await update.message.reply_html(
+            'Выберите подходящее место и отправьте число от 1 до 10,'
+            ' чтобы получить точную информацию об этом месте',
+            reply_markup=select_kb_reply
+        )
+
+        return 3
 
 
 async def get_name_information(update, context):
+    select = str(int(update.message.text) - 1)
+    organization = context.user_data['list_of_some'][select]
+
+    context.user_data['list_of_some'] = {}
+
+    org_name = organization["properties"]["CompanyMetaData"]["name"]
+    org_address = organization["properties"]["CompanyMetaData"]["address"]
+    org_coordinates = organization["geometry"]["coordinates"]
+    org_map = f'https://static-maps.yandex.ru/1.x/?ll=' \
+              f'{str(organization["geometry"]["coordinates"][0])},{str(organization["geometry"]["coordinates"][1])}' \
+              f'&spn=0.005,0.005&l=map&pt={str(organization["geometry"]["coordinates"][0])},' \
+              f'{str(organization["geometry"]["coordinates"][1])},pm2rdl'
+
+    answer = f'''
+    {org_name}
+    Адрес: {org_address}
+    Точные координаты: {str(org_coordinates).rstrip("]").lstrip("[")}'''
+
+    await update.message.reply_photo(org_map, caption=answer)
+
+    return ConversationHandler.END
+
+
+async def get_address_information(update, context):
     select = str(int(update.message.text) - 1)
     organization = context.user_data['list_of_some'][select]
 
@@ -120,13 +169,6 @@ async def help_command(update, context):
     )
 
 
-async def close_keyboard(update, context):
-    await update.message.reply_text(
-        "Хорошо, я могу закрыть панель",
-        reply_markup=ReplyKeyboardRemove()
-    )
-
-
 async def stop(update, context):
     await update.message.reply_text("Диалог остановлен.")
     return ConversationHandler.END
@@ -136,11 +178,12 @@ def main():
     application = Application.builder().token(BOT_TOKEN).build()
 
     with_name = ConversationHandler(
-        entry_points=[CommandHandler('start_name', start_name)],
+        entry_points=[CommandHandler('start', start)],
 
         states={
-            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_name)],
-            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_name_information)]
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_name_or_address)],
+            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_name_information)],
+            3: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_address_information)]
         },
         fallbacks=[CommandHandler('stop', stop)]
     )
@@ -149,8 +192,6 @@ def main():
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("close", close_keyboard))
-    application.add_handler(CommandHandler("start_name", start_name))
     application.add_handler(CommandHandler("stop", stop))
 
     application.run_polling()
